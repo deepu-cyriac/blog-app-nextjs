9d80297ff7f2765a85f1dd575cbf75c93960f980
Deepu Cyriac <deepucvk123@gmail.com>
2025-01-27 09:43:07 +0530

    Added content for post detail page

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,22 +3,23 @@ import PostHeader from "./post-header";
 
 import ReactMarkDown from "react-markdown";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs1",
-  title: "Getting started with Next.js",
-  image: "getting-started-nextjs.png",
-  excerpt:
-    "NextJS is a React framework for making fullstack React applications.",
-  date: "2025-01-24",
-  content: "# This is a first post",
-};
+// const DUMMY_POST = {
+//   slug: "getting-started-with-nextjs1",
+//   title: "Getting started with Next.js",
+//   image: "getting-started-nextjs.png",
+//   excerpt:
+//     "NextJS is a React framework for making fullstack React applications.",
+//   date: "2025-01-24",
+//   content: "# This is a first post",
+// };
 
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkDown>{DUMMY_POST.content}</ReactMarkDown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkDown>{post.content}</ReactMarkDown>
     </article>
   );
 }
