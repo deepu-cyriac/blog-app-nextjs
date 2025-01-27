8b8f893f1bf65659f82ff43631ff3dba02c74c33
Deepu Cyriac <deepucvk123@gmail.com>
2025-01-27 10:00:38 +0530

    Added some markdown code - error image not loading

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 
@@ -15,11 +16,45 @@ import ReactMarkDown from "react-markdown";
 
 function PostContent(props) {
   const { post } = props;
+
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkDown>{post.content}</ReactMarkDown>
+      <ReactMarkDown components={customRenderers}>{post.content}</ReactMarkDown>
     </article>
   );
 }
